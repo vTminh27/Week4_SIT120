813a36727fdb9127fd85ade12e7bf8ac4a35b5b8
Minh Vu <102584254+vTminh27@users.noreply.github.com>
2022-09-13 01:51:51 +1000

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <Navigation />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navigation from './components/Navigation';
+export default {
+  name: "App",
+  components: { 
+    Navigation,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Raleway", sans-serif;
+  font-weight: 400;
+}
+
+.app {
+  min-height: 100vh;
+  position: relative;
+  background-color: #f1f1f1;
+}
+.container {
+  padding: 0 20px;
+  max-width: 1140px;
+  margin: 0 auto;
+}
+</style>
